3ff2b7c85debeec2e71d16aee9260b3bc0c93a4b
mp <mp.redacted@denodo.com>
2015-06-08 09:17:16 +0200

    Added some query string options

--- denodo-odata-server-5.5/src/main/java/com/denodo/connect/business/entities/entityset/repository/EntityRespository.java
+++ denodo-odata-server-5.5/src/main/java/com/denodo/connect/business/entities/entityset/repository/EntityRespository.java
@@ -10,17 +10,16 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.apache.log4j.Logger;
 import org.apache.olingo.odata2.api.edm.EdmException;
 import org.apache.olingo.odata2.api.edm.EdmNavigationProperty;
-import org.apache.olingo.odata2.api.edm.provider.NavigationProperty;
 import org.apache.olingo.odata2.api.uri.NavigationSegment;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
-
-import com.denodo.connect.DenodoEdmProvider;
 
 @Repository
 public class EntityRespository {
@@ -30,23 +29,22 @@ public class EntityRespository {
     private static final Logger logger = Logger.getLogger(EntityRespository.class);
 
     public List<Map<String, Object>> getEntitySet(final String entitySetName, final String orderByExpressionString, final Integer top,
-            final Integer skip, final String filterExpression) throws SQLException {
-
-        return getEntityData(entitySetName, null, orderByExpressionString, top, skip, filterExpression,null,null);
+            final Integer skip, final String filterExpression, final List<String> selectedItems) throws SQLException {
+        return getEntityData(entitySetName, null, orderByExpressionString, top, skip, filterExpression,selectedItems,null,null);
     }
 
     public Map<String, Object> getEntity(final String entityName, final Map<String, Object> keys) throws SQLException {
-
-        return getEntityData(entityName, keys, null, null, null, null,null,null).get(0);
+        return getEntityData(entityName, keys, null, null, null, null, null, null, null).get(0);
     }
 
     public Map<String, Object> getEntityByAssociation(final String entityName, final Map<String, Object> keys,List<NavigationSegment> navigationSegments, String tableTarget) throws SQLException {
 
-        return getEntityData(entityName, keys, null, null, null, null,navigationSegments,tableTarget).get(0);
+        return getEntityData(entityName, keys, null, null, null, null, null,navigationSegments,tableTarget).get(0);
     }
     
     private List<Map<String, Object>> getEntityData(final String entityName, final Map<String, Object> keys,
-            final String orderByExpression, final Integer top, final Integer skip, final String filterExpression, final List<NavigationSegment> navigationSegments, final String tableTarget) throws SQLException {
+            final String orderByExpression, final Integer top, final Integer skip, final String filterExpression, final List<String> selectedItems, final List<NavigationSegment> navigationSegments, final String tableTarget) throws SQLException {
+
         Connection jdbcConnection = null;
         List<Map<String, Object>> entitySetData = new ArrayList<Map<String, Object>>();
 
@@ -55,7 +53,13 @@ public class EntityRespository {
 
             ResultSet resultSet = null;
 
-            String sqlStatement = getSQLStatement(entityName, keys, filterExpression, navigationSegments, tableTarget);
+            String filterExpressionAdapted = getSubstringofOption(filterExpression);
+            filterExpressionAdapted = getStartsWithOption(filterExpressionAdapted);
+            filterExpressionAdapted = getIndexOfOption(filterExpressionAdapted);
+
+            String selectExpression = getSelectOption(selectedItems);
+
+            String sqlStatement = getSQLStatement(entityName, keys, filterExpressionAdapted, selectExpression, navigationSegments, tableTarget);
             sqlStatement = addOrderByExpression(sqlStatement, orderByExpression);
             sqlStatement = addTopOption(sqlStatement, top);
             sqlStatement = addSkipOption(sqlStatement, skip);
@@ -70,11 +74,13 @@ public class EntityRespository {
             while (resultSet.next()) {
                 ResultSetMetaData resultSetMetaData = resultSet.getMetaData();
                 Map<String, Object> rowData = new HashMap<String, Object>();
+
                 for (int i = 1; i <= resultSetMetaData.getColumnCount(); i++) {
                     String columnName = resultSetMetadata.getColumnName(i);
 
                     rowData.put(columnName, resultSet.getObject(i));
                 }
+                
                 entitySetData.add(rowData);
             }
 
@@ -89,7 +95,7 @@ public class EntityRespository {
         return entitySetData.isEmpty() ? null : entitySetData;
     }
 
-    private static String getSQLStatement(final String viewName, final Map<String, Object> keys, final String filterExpression, List<NavigationSegment> navigationSegments, String tableTarget) throws EdmException {
+    private static String getSQLStatement(final String viewName, final Map<String, Object> keys, final String filterExpression, final String selectExpression, List<NavigationSegment> navigationSegments, String tableTarget) throws EdmException {
 
         StringBuilder sb = new StringBuilder();
     
@@ -102,8 +108,14 @@ public class EntityRespository {
         		sb.append(" LEFT JOIN "+navigationProperty.getName()+" ON "+navigationProperty.getMapping().getInternalName()+"="+navigationProperty.getMapping().getMediaResourceSourceKey());
         	}	
         }else{
-            sb.append("SELECT "+viewName+".* FROM ");
-        	sb.append(viewName);	
+        	sb.append("SELECT ");
+            if (!selectExpression.isEmpty()) {
+                sb.append(selectExpression);
+            } else {
+                sb.append(viewName).append(".* ");
+            }
+            sb.append("FROM ");
+            sb.append(viewName);
         }
 
         boolean whereClause = false;
@@ -127,7 +139,7 @@ public class EntityRespository {
                 first = false;
             }
         }
-        
+
         if (filterExpression != null) {
             if (!whereClause) {
                 sb.append(" WHERE ");
@@ -174,8 +186,110 @@ public class EntityRespository {
                 sb.append(" OFFSET ");
                 sb.append(skip);
                 sb.append(" ROWS");
+                // TODO: This must be removed when the Task #22418 - Problem
+                // with OFFSET clause is resolved
+                sb.append(" LIMIT ");
+                sb.append(Integer.MAX_VALUE);
             }
         }
+        return sb.toString();
+    }
+
+    private static String getSubstringofOption(final String filterExpression) {
+        if (filterExpression != null) {
+            final Pattern pattern = Pattern.compile("(.*)(substringof)\\((')(\\w+)('),(\\w+)\\)( eq true| eq false)?(.*)");
+
+            final Matcher matcher = pattern.matcher(filterExpression);
+            if (!matcher.find()) {
+                return filterExpression;
+            }
+
+            StringBuilder newFilterExpression = new StringBuilder();
+            matcher.reset();
+            while (matcher.find()) {
+                newFilterExpression.append(matcher.group(1));
+
+                final String substring = matcher.group(4);
+                final String columnName = matcher.group(6);
+                final String condition = matcher.group(7);
+
+                newFilterExpression.append(columnName).append(getCondition(condition)).append("'%").append(substring).append("%'");
+                newFilterExpression.append(matcher.group(8));
+            }
+
+            return newFilterExpression.toString();
+        }
+        return filterExpression;
+    }
+
+    private static String getStartsWithOption(final String filterExpression) {
+        if (filterExpression != null) {
+            final Pattern pattern = Pattern.compile("(.*)(startswith)\\((\\w+),(')(\\w+)(')\\)( eq true| eq false)?(.*)");
+
+            final Matcher matcher = pattern.matcher(filterExpression);
+            if (!matcher.find()) {
+                return filterExpression;
+            }
+
+            StringBuilder newFilterExpression = new StringBuilder();
+            matcher.reset();
+            while (matcher.find()) {
+                newFilterExpression.append(matcher.group(1));
+
+                final String substring = matcher.group(5);
+                final String columnName = matcher.group(3);
+                final String condition = matcher.group(7);
+
+                newFilterExpression.append(columnName).append(getCondition(condition)).append("'").append(substring).append("%'");
+                newFilterExpression.append(matcher.group(8));
+            }
+
+            return newFilterExpression.toString();
+        }
+        return filterExpression;
+    }
+
+    private static String getIndexOfOption(final String filterExpression) {
+        if (filterExpression != null) {
+            final Pattern pattern = Pattern.compile("(.*)(indexof)(.*)");
+
+            final Matcher matcher = pattern.matcher(filterExpression);
+            if (!matcher.find()) {
+                return filterExpression;
+            }
+
+            StringBuilder newFilterExpression = new StringBuilder();
+            matcher.reset();
+            while (matcher.find()) {
+                newFilterExpression.append(matcher.group(1)).append("INSTR").append(matcher.group(3));
+            }
+
+            return newFilterExpression.toString();
+        }
+        return filterExpression;
+    }
+
+    private static String getCondition(final String condition) {
+
+        final String falseCondition = " eq false";
+        if (falseCondition.equals(condition)) {
+            return " NOT LIKE ";
+        }
+        return " LIKE ";
+    }
+
+    private static String getSelectOption(final List<String> selectedItems) {
+        StringBuilder sb = new StringBuilder();
+
+        for (String item : selectedItems) {
+            sb.append(item).append(",");
+        }
+
+        if (sb.length() > 0) {
+            sb.insert(0, " ");
+            sb.replace(sb.length() - 1, sb.length(), " ");
+        }
+
         return sb.toString();
     }
 }
--- denodo-odata-server-5.5/src/main/java/com/denodo/connect/business/services/entity/impl/EntityServiceImpl.java
+++ denodo-odata-server-5.5/src/main/java/com/denodo/connect/business/services/entity/impl/EntityServiceImpl.java
@@ -1,10 +1,14 @@
 package com.denodo.connect.business.services.entity.impl;
 
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
+
 import org.apache.olingo.odata2.api.uri.NavigationSegment;
+import org.apache.olingo.odata2.api.edm.EdmException;
+import org.apache.olingo.odata2.api.uri.SelectItem;
 import org.apache.olingo.odata2.api.uri.expression.FilterExpression;
 import org.apache.olingo.odata2.api.uri.expression.OrderByExpression;
 import org.apache.olingo.odata2.api.uri.info.GetEntitySetUriInfo;
@@ -15,43 +19,64 @@ import com.denodo.connect.business.entities.entityset.repository.EntityResposito
 import com.denodo.connect.business.services.entity.EntityService;
 
 @Service
-public class EntityServiceImpl implements EntityService{
+public class EntityServiceImpl implements EntityService {
 
     @Autowired
     EntityRespository entityRespository;
-    
+
     public EntityServiceImpl() {
         super();
     }
 
     @Override
-    public  List<Map<String, Object>> getEntitySet(final String entitySetName, final GetEntitySetUriInfo uriInfo) throws SQLException {
-        
+    public List<Map<String, Object>> getEntitySet(final String entitySetName, final GetEntitySetUriInfo uriInfo) throws SQLException {
+
         // Orderby System Query Option ($orderby)
-        OrderByExpression orderByExpression  = uriInfo.getOrderBy();
+        OrderByExpression orderByExpression = uriInfo.getOrderBy();
         String orderByExpressionString = null;
         if (orderByExpression != null) {
             orderByExpressionString = orderByExpression.getExpressionString();
         }
-        
+
         // Top System Query Option ($top)
         Integer top = uriInfo.getTop();
-        
+
         // Skip System Query Option ($skip)
         Integer skip = uriInfo.getSkip();
-        
+
         // Filter System Query Option ($filter)
         FilterExpression filterExpression = uriInfo.getFilter();
         String filterExpressionString = null;
         if (filterExpression != null) {
             filterExpressionString = filterExpression.getExpressionString();
         }
-        
-        return this.entityRespository.getEntitySet(entitySetName, orderByExpressionString, top, skip, filterExpressionString);
+
+        // Select System Query Option ($select)
+        List<SelectItem> selectedItems = uriInfo.getSelect();
+        List<String> selectedItemsAsString = getSelectOptionValues(selectedItems);
+
+        return this.entityRespository.getEntitySet(entitySetName, orderByExpressionString, top, skip, filterExpressionString,
+                selectedItemsAsString);
+    }
+
+    private static List<String> getSelectOptionValues(final List<SelectItem> selectedItems) {
+        List<String> selectValues = new ArrayList<String>();
+
+        for (SelectItem item : selectedItems) {
+            try {
+                selectValues.add(item.getProperty().getName());
+            } catch (EdmException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+
+
+        return selectValues;
     }
     
     @Override
-    public  Map<String, Object> getEntity(final String entityName, final Map<String, Object> keys) throws SQLException {
+    public Map<String, Object> getEntity(final String entityName, final Map<String, Object> keys) throws SQLException {
         return this.entityRespository.getEntity(entityName, keys);
     }
 

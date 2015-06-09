5cd1c699efb0e75417a2feccae56eee216152cb9
df <df.redacted@denodo.com>
2015-06-09 19:26:42 +0200

    Added license headers to all source code files

--- denodo-odata-server-5.5/src/main/java/com/denodo/connect/business/services/entity/impl/EntityServiceImpl.java
+++ denodo-odata-server-5.5/src/main/java/com/denodo/connect/business/services/entity/impl/EntityServiceImpl.java
@@ -1,3 +1,24 @@
+/*
+ * =============================================================================
+ *
+ *   This software is part of the DenodoConnect component collection.
+ *
+ *   Copyright (c) 2014-2015, denodo technologies (http://www.denodo.com)
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ *
+ * =============================================================================
+ */
 package com.denodo.connect.business.services.entity.impl;
 
 import java.sql.SQLException;
@@ -108,7 +129,7 @@ public class EntityServiceImpl implements EntityService {
 			List<NavigationSegment> navigationSegments, String tableTarget, EdmProperty property)
 			throws SQLException {
 
-		  return this.entityRepository.getEntityByAssociation(entityName, keys,navigationSegments,tableTarget, property);
+		  return this.entityRepository.getEntityByAssociation(entityName, keys, navigationSegments, tableTarget, property);
 	}
 	@Override
 	public Map<String, Object> getEntityAssociation(String entityName,
--- denodo-odata-server-5.5/src/main/java/com/denodo/connect/business/services/metadata/impl/MetadataServiceImpl.java
+++ denodo-odata-server-5.5/src/main/java/com/denodo/connect/business/services/metadata/impl/MetadataServiceImpl.java
@@ -1,13 +1,23 @@
 /*
- * @(#)CommunicationsServiceImpl.java
+ * =============================================================================
  *
- * Copyright (c) 2.009, denodo technologies, S.L. All Rights Reserved.
+ *   This software is part of the DenodoConnect component collection.
  *
- * This software is the confidential and proprietary information of
- * denodo technologies, S.L. ("Confidential Information"). You shall not
- * disclose such Confidential Information and shall use it only in
- * accordance with the terms of the license agreement you entered into
- * with denodo technologies S.L.
+ *   Copyright (c) 2014-2015, denodo technologies (http://www.denodo.com)
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ *
+ * =============================================================================
  */
 package com.denodo.connect.business.services.metadata.impl;
 

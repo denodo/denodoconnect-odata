f73099a4abbaced85a740b48fcccce50b41c5fd3
pl <pl.redacted@denodo.com>
2015-09-04 14:10:14 +0200

    Change version of spring to be compatible with Tomcat 5.5 , to deploy the service in the embedded tomcat of the platform. Include the necessary scripts to deploy the service in the embedded web container.

---
+++ denodo-odata2-service-5.5/src/main/scripts/odata_service_shutdown.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# -----------------------------------------------------------------------------
+#  Environment variable JAVA_HOME must be set and exported
+# -----------------------------------------------------------------------------
+
+# -----------------------------------
+#  DENODO_HOME variable must be set!
+# -----------------------------------
+DENODO_HOME="/opt/denodo"
+
+export DENODO_CONF="$DENODO_HOME/conf/vdp"
+
+exec "$DENODO_HOME/bin/webcontainer.sh" stop denodo-odata2-service-5.5
+
